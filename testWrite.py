e8b5644e6dd08036e601f8b7380b577457e17534
Tim Savannah <kata198@gmail.com>
2017-01-06 14:21:02 -0500

    Update testWrite.py to give average write speed and call os.sync and short sleep between tests, if a flag is set. Also add some more documentation to testWrite.py

--- testWrite.py
+++ testWrite.py
@@ -4,6 +4,8 @@
 
     The testcase itself is hereby granted access as a Public Domain work, or the closest legally in your area.
 
+    This tests the various priority levels of BackgroundWrite and gives output based on results.
+
     You can use this to experiment with different values and priority levels on your hardware and use cases.
 
     It will perform some math/memory operations whilst writing 2 files using chains at the same time.
@@ -11,6 +13,14 @@
     "interactivity score". The interactivity score is the number of calculations divided by the time you spent writing.
     The higher the ioPrio, the higher the interactivity score should be. It is a measure of how much OTHER work got done while you were
     performing otherwise blocking I/O operations.
+
+    You may consider having a "normal" background workload going before running this test, so you can guage for yourself
+      what interactivity level to choose with BackgroundWrite depending on if you want to maximize throughput of your write,
+      or cpu-bound / other processes.
+
+    There is a "DO_SYNC" option, that if True will cause the drive to flush all data to disk. You may want to play with this on
+    and off. For example, if you are running a dd if=/dev/urandom of=testfile bs=512 to simulate background I/O on the same device,
+    it will flush both datasets. This is moreso intended to be set to True when NO background I/O is going.
 '''
 import os
 import glob
@@ -27,6 +37,12 @@ class dummy(object):
 
 
 # Pick a big file
+
+# Set to True to sync ALL I/O (this process and others) to disk before each test.
+#  This should be True if no background I/O is being ran, and maybe True maybe False if some is running.
+#  It will make tests more "fair" between eachother, but with various background tasks it may introduce
+#  a large variance and margin of error.
+DO_SYNC = True
 
 if __name__ == '__main__':
 
@@ -56,6 +72,8 @@ if __name__ == '__main__':
     # Expand that big data
     data = data * 50
 
+    dataLen = len(data)
+
     # Iterate through the I/O priorities, do the operation, and show the score.
     for ioPrio in range(startPrio, endPrio, 1):
         answers = [22]
@@ -67,6 +85,12 @@ if __name__ == '__main__':
             os.unlink('/home/%s/nb_test_output2' %(username,))
         f = open('/home/%s/nb_test_output1' %(username,), 'wb')
         f2 = open('/home/%s/nb_test_output2' %(username,), 'wb')
+
+        if DO_SYNC is True:
+            # Ensure pending data is flushed so next run has fair chance
+            os.sync()
+            time.sleep(3)
+
 
         before = time.time()
         # Start first chain, final will close f1
@@ -99,9 +123,11 @@ if __name__ == '__main__':
         sys.stdout.write(('-' * 40) + '\n')
         sys.stdout.write('[%d] Time to write: %f\n' %(ioPrio, delta) )
         sys.stdout.write('[%d] Number of answers generated: %d\n' %(ioPrio, numAnswers) )
+        sys.stdout.write('[%d] Average write speed: %f M/s\n' %(ioPrio, round((dataLen / delta) / (1024.0 * 1024.0), 5) ) )
         sys.stdout.write('[%d] Interactivity score: %f\n' % (ioPrio, round(numAnswers / delta, 5)) )
         sys.stdout.write(('=' * 40) + '\n\n')
         sys.stdout.flush()
+
 
 
     if os.path.exists('/home/%s/nb_test_output1' %(username,)):
