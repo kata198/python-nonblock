8ca80afcdd116b0dfc801e95e75fb228a00c0fbf
Tim Savannah <kata198@gmail.com>
2018-01-01 02:32:24 -0500

    Update copyright notice

--- nonblock/read.py
+++ nonblock/read.py
@@ -1,5 +1,5 @@
 '''
-    Copyright (c) 2015-2016 Timothy Savannah under terms of LGPLv2. You should have received a copy of this LICENSE with this distribution.
+    Copyright (c) 2015-2018 Timothy Savannah under terms of LGPLv2. You should have received a copy of this LICENSE with this distribution.
 
     read.py Contains pure-python functions for non-blocking reads in python
 
