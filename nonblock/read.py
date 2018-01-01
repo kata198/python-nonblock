269322fddf52d5c95b6e6010479481cf3a4eaf13
Tim Savannah <kata198@gmail.com>
2018-01-01 02:13:49 -0500

    Add a dev-level impl to nonblock_read which can use getbuffn if available on passed stream. See comments added to function for a lot of info.

--- nonblock/read.py
+++ nonblock/read.py
@@ -6,7 +6,7 @@
 	
 '''
 # vim: ts=4 sw=4 expandtab
-
+import sys
 import select
 
 from .common import detect_stream_mode
@@ -46,6 +46,71 @@ def nonblock_read(stream, limit=None, forceMode=None):
     else:
         raise ValueError('Cannot determine how to read from provided stream, %s.' %(repr(stream),))
 
+    # See if this is a buffered stream which supports the "getbuffn" call.
+    #    getbuffn is a yet-to-be-submitted/accepted additional call in the
+    #     python api for a BufferedReader stream.
+    #
+    # It returns the number of bytes which are waiting to be returned in the
+    #   underlying stream buffer.
+    #
+    # On a typical linux filesystem which uses 4096 byte I/O blocks, the "read"
+    #  call will end up reading 4096 bytes, yet we read just 1,
+    #   in order to prevent blocking (by selecting each byte).
+    #
+    #  This means that for a filesystem non-blocking I/O read, we are basically
+    #    adding the unnecessary overhead of a select syscall, a call to the python read,
+    #    and all the associated overhead of those functions and going from C-mode to Py-mode
+    #   4095 times out of 4096.
+    #
+    #  Also helps a lot with scheduling on a loaded system as we are limiting the amount of
+    #   times we allow ourselves to be pre-empted
+    #
+    #  With the addition of "getbuffn" we are able to suck up that full buffer in one swing.
+    #
+    #   On a 5 meg file from a VM which is running on an SSD, I average the following:
+    #
+    #    Loaded system, non-cached I/O:
+    #
+    #       One-Fell-Swoop file.read() - .3 seconds
+    #
+    #       getbuffn-patched python and this impl - 3.1 seconds
+    #
+    #       unpatched python and this impl - 41 to 55 = 44 seconds. ( avg min - avg max)
+    #
+    #    Unloaded system, cached I/O:
+    #
+    #       One-Fell-Swoop file.read() - .0017 seconds
+    #
+    #       getbuffn-patched python and this impl - .034 seconds
+    #
+    #       unpatched python and this impl - 45 seconds. ( not as variable as loaded system )
+    #
+    #
+    #  TODO:
+    #       - This call has been "hacked-in" with the existing function-flow
+    #          for ease of analysis and comparison. In reality, we should be
+    #          calling getbuffn AFTER we read the single byte, which will
+    #          save a loop iteration and a call to select
+    #
+    #       - Currently we ignore the #limit parameter and may over-read with
+    #          this impl. We should thus choose to read the minimum of
+    #          available bytes (getbuffn ret) and (limit - bytesRead)
+
+    #if True:
+    #    hasGetBuffN = False   
+    #elif hasattr(stream, 'getbuffn'):
+    if hasattr(stream, 'getbuffn'):
+        print ( "getbuffn detected (direct)" )
+        hasGetBuffN = True
+        getbuffn = stream.getbuffn
+    elif hasattr(stream, 'buffer') and hasattr(stream.buffer, 'getbuffn'):
+        print ( "getbuffn detected (on .buffer obj)" )
+        hasGetBuffN = True
+        getbuffn = stream.buffer.getbuffn
+    else:
+        print ( "No 3 ")
+        hasGetBuffN = False
+
     while True:
         # Check if data on stream is immediately available
         (readyToRead, junk1, junk2) = select.select([stream], [], [], .000001)
@@ -53,7 +118,29 @@ def nonblock_read(stream, limit=None, forceMode=None):
         if not readyToRead:
             break
 
-        c = readByte()
+        numToRead = 1
+
+        if hasGetBuffN:
+            availableBytes = getbuffn()
+
+            if availableBytes > 0:
+                #sys.stderr.write('Got %d buffered bytes!\n' %(availableBytes, ))
+                #sys.stderr.flush()
+                numToRead = availableBytes
+                
+
+        if numToRead == 1:
+            c = readByte()
+        else:
+            # TODO: Should we be calling "read" or "read1" here?
+            #         "read1" seems to make the most sense conceptually,
+            #         but based on readahead maybe "read" could be better?
+            #
+            #         "read" does seem to test slightly better than read1
+            #            on an SSD
+            c = stream.read(numToRead)
+            #c = stream.read1(numToRead)
+
         if c == emptyStr:
             # Stream has been closed
             if not ret:
@@ -62,7 +149,7 @@ def nonblock_read(stream, limit=None, forceMode=None):
             # Otherwise, return data collected. Next call will return None.
             break
 
-        bytesRead += 1
+        bytesRead += numToRead
         ret.append(c)
 
         if limit and bytesRead >= limit:
